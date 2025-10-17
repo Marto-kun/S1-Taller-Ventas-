4dcfcb6692c327c6c9f72df7b2c3213de9fdd820
Marto-kun <martinvozme456@gmail.com>
2025-10-17 12:37:11 -0500

    Creacion de funciones

--- SistemaDeVentas.c
+++ SistemaDeVentas.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdbool.h>
+// Declaración de variables:
+int precioUn, totalVenta, stock, prodId, sumStock; // Variables del programa
+int opc1;                                          // Variables del menu
+char prod[20];                                     // Limitar los caracteres del nombre del producto
+bool registro = false;                             // Variable para verificar si hay un producto registrado
 
-int main() 
+// Declaracion de funciones
+void ConsultaStock(); // Funcion caso 2
+void Reabastecer();   // Funcion caso 3
+void Venta();         // Funcion caso 4
+
+int main()
 {
-
-    // Declaración de variables:
-    int precioUn, totalVenta, stock, prodId, sumStock; // Variables del programa
-    int opc1;                                          // Variables del menu
-    char prod[20];                                     // Limitar los caracteres del nombre del producto
-    bool registro = false;                             // Variable para verificar si hay un producto registrado
 
     // Menu principal del sistema
     do
@@ -25,7 +29,7 @@ int main()
 
         switch (opc1)
         {
-        case 1: // Registro del producto
+        case 1:                   // Registro del producto
             if (registro == true) // Validar si el prodcuto ya fue registrado
             {
                 printf("\nProducto ya registrado, no se pueden registrar nuevos productos. Regresando al menu principal...");
@@ -65,73 +69,16 @@ int main()
             }
             break;
 
-        case 2:                    // Consulta de Stock
-            if (registro == false) // Verificacion de existencia de producto
-            {
-                printf("\nNo se ha registrado ningun producto... Regresando al menu principal...");
-            }
-            else
-            {
-                printf("\nEl stock actual del producto es: %i", stock);
-                break;
-            }
+        case 2: // Consulta de Stock
+            ConsultaStock();
             break;
 
-        case 3:                    // Reabastecimiento de Stock
-            if (registro == false) // Verificacion de existencia de producto
-            {
-                printf("\nNo se ha registrado ningun producto... Regresando al menu principal...");
-                break;
-            }
-            else
-            {
-                do
-                {
-                    printf("\nIngrese la cantidad que desea añadir al stock (debe ser mayor a 0): ");
-                    fflush(stdin);
-                    scanf("%i", &sumStock);
-
-                } while (sumStock <= 0);
-
-                stock += sumStock;
-                printf("Stock actualizado existosamente. Nuevo Stock: %i", stock);
-                break;
-            }
+        case 3: // Reabastecimiento de Stock
+            Reabastecer();
             break;
-        case 4:                    // Venta del producto
-            if (registro == false) // Verificacion de existencia de producto
-            {
-                printf("\nNo se ha registrado ningun producto... Regresando al menu principal...");
-                break;
-            }
-            else
-            {
-                do
-                {
-                    printf("\nIngrese la cantidad que desea vender: ");
-                    fflush(stdin);
-                    scanf("%i", &totalVenta);
-                    if (totalVenta <= 0)
-                    {
-                        printf("\nCantidad inválida. Debe ser mayor a 0.");
-                    }
-                } while (totalVenta <= 0);
 
-                while (stock < totalVenta)
-                {
-                    printf("\nNo hay suficiente stock para realizar la venta.");
-                    printf("\nIntentelo de nuevo...");
-                    printf("\nIngrese la cantidad que desea vender: ");
-                    fflush(stdin);
-                    scanf("%i", &totalVenta);
-                }
-
-                stock -= totalVenta;
-                printf("\nVenta realizada exitosamente.");
-                printf("\nGanancia obtenida: $%i", totalVenta * precioUn);
-
-                break;
-            }
+        case 4: // Venta del producto
+            Venta();
             break;
 
         case 5: // Salida del programa
@@ -144,4 +91,73 @@ int main()
         }
 
     } while (opc1 != 5);
+}
+
+// Codificacion de Funciones
+
+void ConsultaStock()
+{
+    if (registro == false) // Verificacion de existencia de producto
+    {
+        printf("\nNo se ha registrado ningun producto... Regresando al menu principal...");
+    }
+    else
+    {
+        printf("\nEl stock actual del producto es: %i", stock);
+    }
+}
+
+void Reabastecer()
+{
+    if (registro == false) // Verificacion de existencia de producto
+    {
+        printf("\nNo se ha registrado ningun producto... Regresando al menu principal...");
+    }
+    else
+    {
+        do
+        {
+            printf("\nIngrese la cantidad que desea añadir al stock (debe ser mayor a 0): ");
+            fflush(stdin);
+            scanf("%i", &sumStock);
+
+        } while (sumStock <= 0);
+
+        stock += sumStock;
+        printf("Stock actualizado existosamente. Nuevo Stock: %i", stock);
+    }
+}
+
+void Venta()
+{
+    if (registro == false) // Verificacion de existencia de producto
+    {
+        printf("\nNo se ha registrado ningun producto... Regresando al menu principal...");
+    }
+    else
+    {
+        do
+        {
+            printf("\nIngrese la cantidad que desea vender: ");
+            fflush(stdin);
+            scanf("%i", &totalVenta);
+            if (totalVenta <= 0)
+            {
+                printf("\nCantidad inválida. Debe ser mayor a 0.");
+            }
+        } while (totalVenta <= 0);
+
+        while (stock < totalVenta)
+        {
+            printf("\nNo hay suficiente stock para realizar la venta.");
+            printf("\nIntentelo de nuevo...");
+            printf("\nIngrese la cantidad que desea vender: ");
+            fflush(stdin);
+            scanf("%i", &totalVenta);
+        }
+
+        stock -= totalVenta;
+        printf("\nVenta realizada exitosamente.");
+        printf("\nGanancia obtenida: $%i", totalVenta * precioUn);
+    }
 }
